0cf4d6da0e797aa89844a8f9d69f0e242f5eb4cd
Nguyen Van Thieu B <nguyen.van.thieub@framgia.com>
2016-07-11 08:38:07 +0700

    004 - UI Search

---
+++ crbios/Classes/ViewController/SearchViewController/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  crbios
+//
+//  Created by  on 7/11/16.
+//  Copyright © 2016 framgia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
